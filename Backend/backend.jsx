05bf938e77499edcbae2b02bfb74c16b68a78a16
Eugenepython <50318257+Eugenepython@users.noreply.github.com>
2023-09-28 21:59:16 +0100

    first commit

---
+++ Backend/backend.jsx
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { Pool } = require('pg'); 
+
+const path = require('path');
+const dotenvPath = path.join(__dirname, '..', '.env');
+require('dotenv').config({ path: dotenvPath });
+
+
+const thePool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+
+
+const PORT = process.env.PG_PORT || 3000;
+
+console.log(thePool)
+
+let theValue = 'default value'
+
+thePool.query('SELECT * FROM favorite_movies', (error, result) => {
+  if (error) {
+    console.error('Error executing query:', error);
+  } else {
+    console.log('Query result:', result.rows);
+  }
+});
+
+
+console.log(theValue)
+app.use(cors());
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+    console.log('Received POST request:', req.body);
+    //res.send({ message: 'ok', data: req.body }); 
+    const valueToStore = req.body;
+    console.log('valueToStore:', valueToStore.director);
+    thePool.query(
+    'INSERT INTO favorite_movies (title, director, release_year) VALUES ($1, $2, $3)', 
+    [valueToStore.title, valueToStore.director, valueToStore.year],
+    (error, result) => {
+      if (error) {
+        console.error('Error executing query:', error);
+        res.status(500).send({ error: 'An error occurred' });
+      } else {
+        console.log('Query result:', result.rows);
+        res.send({ message: 'ok', data: req.body });
+      }
+    });
+  });
+
+  app.get('/submit', (req, res) => {
+    // Handle the GET request logic here
+    res.send(theValue);
+  });
+  
+
+app.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+});
+
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const [tValue, setTValue] = useState('')
+  const [dValue, setDValue] = useState('')
+  const [yValue, setYValue] = useState('')
+
+  const prodBackendURL = 'https://favoritemoviesbackend-production.up.railway.app'
+  const localBackendURL = 'http://localhost:3000'
+
+  
+
+  function handleClick() {
+    fetch(`${prodBackendURL}/submit`,  {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+         title: tValue,
+         director : dValue,
+         year : yValue
+        
+        }),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => console.log(data))
+      .catch(error => {
+        console.error('Error:', error);
+      });
+}
+  
+
+  return (
+    <>
+
+      <h1>what to do</h1>
+
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          handleClick()
+        }
+        }
+      >
+        <input
+          type="text"
+          placeholder="add the movie title"
+          value={tValue}
+          onChange={event => setTValue(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="add the director"
+          value={dValue}
+          onChange={event => setDValue(event.target.value)}
+        />
+        <input
+          type="integer"
+          placeholder="add the year"
+          value={yValue}
+          onChange={event => setYValue(event.target.value)}
+        />
+        <button
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+
+    </>
+  )
+}
+
+export default App
+
